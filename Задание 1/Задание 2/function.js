74fd949da65609b136b92ccdc082beae8147cc59
Dmitry-Hil <decorgran@mail.ru>
2024-06-06 16:00:28 +0300

    Delete Задание 1/Задание 2 directory

--- Задание 1/Задание 2/function.js
+++
@@ -1,5 +0,0 @@
-function showScreenSize() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    alert('Ширина экрана: ' + width + 'px\nВысота экрана: ' + height + 'px');
-}
