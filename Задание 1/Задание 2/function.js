3d7e9ad8731a7a61e453ed418c4d628863c64c15
Dmitry-Hil <decorgran@mail.ru>
2024-06-06 15:59:19 +0300

    Add files via upload

---
+++ Задание 1/Задание 2/function.js
@@ -0,0 +1,5 @@
+function showScreenSize() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    alert('Ширина экрана: ' + width + 'px\nВысота экрана: ' + height + 'px');
+}
