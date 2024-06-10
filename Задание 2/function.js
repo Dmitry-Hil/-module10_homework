440b045e4f1e964c7e6f763063d8b230fc32bbd4
Dmitry-Hil <decorgran@mail.ru>
2024-06-10 10:43:16 +0300

    function.js

--- Задание 2/function.js
+++ Задание 2/function.js
@@ -1,5 +1,5 @@
 function showScreenSize() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
     alert('Ширина экрана: ' + width + 'px\nВысота экрана: ' + height + 'px');
 }
